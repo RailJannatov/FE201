2d75827e7bcd237f675af0a0b80585fbe31b23fd
RailJannatov <railcennetov@gmail.com>
2024-09-16 19:24:39 +0400

    destructing string methods

---
+++ JS(Destructing)/assets/JS/main.js
@@ -0,0 +1,121 @@
+// const human = {
+//   name: "Xeyal",
+//   surname: "Hesenli",
+//   //   age: 29,
+//   assets: {
+//     house: ["Melissa", "Kristal"],
+//     car: { model: "BMW", marka: "", seat: { first: "1" } },
+//   },
+// };
+
+// const surname = human.surname;
+
+// const {
+//   surname: newSurname,
+//   age = 21,
+//   assets: {
+//     car: {
+//       model,
+//       marka = "G30",
+//       seat: { first },
+//     },
+//   },
+//   name: newName = "Elvin",
+// } = human;
+
+// console.log("age", age);
+
+// console.log("newName", newName);
+// console.log("marka", marka);
+
+// console.log("car", car);
+// console.log("marka", marka);
+// console.log(model);
+// console.log(marka);
+// console.log("mashin", mashin);
+// console.log("surname", newSurname);
+
+// console.log("newName", newName);
+
+// const newSurname = human.surname;
+
+// const surname = ''
+
+// const { assets } = human;
+
+// console.log("1", surname);
+// console.log("2", assets);
+// console.log("3", name);
+
+const array = [
+  undefined,
+  5,
+  "Afsana",
+  null,
+  true,
+  false,
+  [1, 2, 3],
+  { name: "Elvin", surname: "Aghayev", assets: { test: "string" } },
+  "BMW",
+];
+
+// const [
+//   first1,
+//   second,
+//   third,
+//   fourth,
+//   five,
+//   six,
+//   seven,
+//   {
+//     assets: { test },
+//   },
+// ] = array;
+// console.log(test);
+
+// const [, , , , , , [first]] = array;
+
+// console.log(first);
+
+// console.log(array[4]);
+
+const fullName = "Lamiye Kazimli Lami";
+// console.log(fullName.length);
+
+// console.log(fullName.indexOf("W"));
+
+// function CustomIndex(str, target) {
+//   for (let i = 0; i < str.length; i++) {
+//     if (target === str[i]) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// console.log(CustomIndex(fullName, "W"));
+
+// console.log(fullName.replace(/Lami/g, "Seri"));
+
+// const inputValue = "             ";
+// console.log(inputValue.length);
+
+// console.log(inputValue.trim().length);
+
+// const text = "this is Innoway";
+
+// const anotherText = "THIS is INNoway";
+
+// console.log(text.toLowerCase() === anotherText.toLowerCase());
+
+// console.log(text.slice(-1, 3));
+
+// const number = 10;
+
+// console.log(typeof number.toString());
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+
+// console.log(text.includes("test"));
+
+// const newData = data.map(({ shipAddress: adress }) => ({ addres }));
