f0bfc6625e1843d1d05a496cd34e90711fee121c
RailJannatov <railcennetov@gmail.com>
2024-09-18 22:44:36 +0400

    rest and spread

--- JS(Destructing)/assets/JS/main.js
+++ JS(Destructing)/assets/JS/main.js
@@ -119,3 +119,103 @@ const fullName = "Lamiye Kazimli Lami";
 // console.log(text.includes("test"));
 
 // const newData = data.map(({ shipAddress: adress }) => ({ addres }));
+
+const car = {
+  marka: "BMW",
+  model: "g40",
+  age: 2019,
+  value: "40000$",
+  teest1: "test",
+  teest1: "test",
+  teest2: "test",
+  teest3: "test",
+  teest4: "test",
+  teest5: "test",
+};
+
+const person = {
+  name: "Ayxan",
+  surname: "Huseynli",
+  tiktokUsername: "ayxan003",
+  age: 15,
+};
+
+// const arrayWithoutName = newArray2.map(({ name, ...rest }) => {
+//   return { ...rest };
+// });
+
+// const arrayWithoutName2 = newArray2.map(({ name, ...rest }) => ({ ...rest }));
+
+// console.log("test", arrayWithoutName);
+// const { name, ...rest } = person;
+
+// const newPerson = { ...rest };
+
+// const car2 = {
+//   marka: "MERCEdes",
+//   model: "c-class",
+// };
+
+// const { details, marka, model, age, ...rest } = car;
+
+// const fruits = ["alma", "armud", "banan"];
+
+// const [first, ...rest] = fruits;
+
+// const myFruits = [...rest, "kivi", "apelsin"];
+
+// console.log("first", myFruits);
+
+// const anotherCar = { ...rest };
+
+// const { marka: mercMarka, ...qalan } = car2;
+
+// const modifiedCar = { ...rest, value: 2019 };
+
+// console.log("details", details);
+// console.log("rest", modifiedCar);
+
+const newArray = [
+  { marka: "test", model: "1", age: 10, fullname: "test", sum: [1, 2, 3, 4] },
+  { marka: "test", model: "1", age: 10, fullname: "test", sum: [2, 5, 3, 4] },
+  { marka: "test", model: "1", age: 10, fullname: "test", sum: [1, 2, 3, 4] },
+  { marka: "test", model: "1", age: 10, fullname: "test", sum: [1, 2, 3, 4] },
+];
+
+const flatMap = newArray.flatMap(({ sum }) => [...sum]);
+
+// console.log(flatMap);
+
+const newwww = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4];
+const newArray2 = [
+  {
+    name: "Ayxan",
+    surname: "Huseynli",
+    tiktokUsername: "ayxan003",
+    age: 15,
+  },
+  {
+    name: "Ayxan",
+    surname: "Huseynli",
+    tiktokUsername: "ayxan003",
+    age: 15,
+  },
+  {
+    name: "Ayxan",
+    surname: "Huseynli",
+    tiktokUsername: "ayxan003",
+    age: 15,
+  },
+  {
+    name: "Ayxan",
+    surname: "Huseynli",
+    tiktokUsername: "ayxan003",
+    age: 15,
+  },
+];
+
+// const concatArray = [...newArray, ...newArray2];
+
+// console.log("concat", concatArray);
+
+// [{  model: "1", age: 10, fullname: "test" },{  model: "1", age: 10, fullname: "test" },{  model: "1", age: 10, fullname: "test" },]
